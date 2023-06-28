1ecb8fc12e74052fe900a126b01b4d5517a88f09
Aleisder <tsarenko_d@inbox.ru>
2023-06-28 18:05:51 +0300

    Initial commit

---
+++ src/main/java/ru/tsarenko/langs/LangsApplication.java
@@ -0,0 +1,13 @@
+package ru.tsarenko.langs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangsApplication.class, args);
+	}
+
+}
